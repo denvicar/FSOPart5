f69982fa9b9713b880a959790ac80bf14ddd0fbe
Ciro Faenza <rkumura3@gmail.com>
2021-11-04 10:54:56 +0100

    exercise5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+const Blog = ({blog}) => {
+  const [detailView, setDetailView] = useState(false)
+  const displayDetail = {display: detailView? '' : 'none'}
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleDetail = () => {
+    setDetailView(!detailView)
+  }
+
+  return(
+    <>
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={toggleDetail}>{detailView ? 'hide' : 'show'}</button>
+    </div> 
+    <div style={displayDetail}>
+      <div style = {blogStyle}>
+      {blog.url} <br />
+      likes {blog.likes} <button>like</button> <br />
+      {blog.user && blog.user.name}
+      </div>
+    </div>
+    </>
+  ) 
+}
 
 export default Blog
