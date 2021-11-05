6859cbdae4b4bcd75b0c4ce295a07c97264686d7
Ciro Faenza <rkumura3@gmail.com>
2021-11-05 13:21:12 +0100

    exercise5.14

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import Blog from './Blog'
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 describe('<Blog />', () => {
@@ -19,5 +19,17 @@ describe('<Blog />', () => {
 
     expect(divGeneral).toHaveTextContent('titolo autore')
     expect(divDetail).toHaveStyle('display: none')
+  })
+
+  test('that detail is shown if button is clicked', () => {
+    const divDetail = component.container.querySelector('.blogDetail')
+    const button = component.getByText('show')
+    fireEvent.click(button)
+
+    expect(divDetail).toBeDefined()
+    expect(divDetail).not.toHaveStyle('display: none')
+    expect(divDetail).toHaveTextContent('url')
+    expect(divDetail).toHaveTextContent('likes 2')
+
   })
 })
