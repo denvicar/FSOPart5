e877c4fa16cf88fee7cdf3854e695d85f5170744
Ciro Faenza <rkumura3@gmail.com>
2021-11-05 16:41:06 +0100

    exercise5.21

--- bloglist-frontend/cypress/integration/blog_app.spec.js
+++ bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -47,14 +47,33 @@ describe('Blog app', function() {
 
   describe('And there is a post', function() {
     beforeEach(function() {
-      cy.login({username: 'cfaenza', password: 'testpass'})
-      cy.createPost({title: 'titolo', author: 'autore', url: 'url'})
+      cy.request('POST', 'http://localhost:3003/api/users', {
+        username: 'mlenders',
+        password: 'testpass2',
+        name: 'Mark Lenders'
+      })
+      cy.login({ username: 'cfaenza', password: 'testpass' })
+      cy.createPost({ title: 'titolo', author: 'autore', url: 'url' })
     })
 
     it('a like can be added', function() {
       cy.contains('show').click()
       cy.contains('like').click()
       cy.get('body').contains('likes 1').should('be.visible')
+    })
+
+    it('the user who added it can delete it', function() {
+      cy.contains('show').click()
+      cy.contains('delete').should('be.visible').click()
+      cy.get('body').should('not.contain','titolo autore')
+    })
+
+    it.only('other users cannot delete it', function() {
+      cy.contains('logout').click()
+      cy.login({ username: 'mlenders', password: 'testpass2' })
+      cy.get('html').should('contain', 'titolo autore')
+      cy.contains('show').click()
+      cy.contains('delete').should('not.be.visible')
     })
   })
 })
